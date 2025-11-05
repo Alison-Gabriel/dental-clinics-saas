ac7ce95bf0c1aa84c28f58ceaa62be04a2b2dd04
Alison Gabriel <odevalison@gmail.com>
2025-11-05 16:10:04 -0300

    feat(hero): add new padding on mobile and new heading, paragraph and button size

--- src/app/(public)/_components/hero.tsx
+++ src/app/(public)/_components/hero.tsx
@@ -5,21 +5,21 @@ import heroImg from "@/public/doctor-hero.png";
 
 export function Hero() {
   return (
-    <section className="bg-white lg:shadow">
-      <div className="container mx-auto px-4 pt-32 sm:px-6 lg:px-8">
+    <section className="bg-white pt-32 pb-16 lg:pb-0">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <main className="flex flex-col items-center justify-center lg:flex-row">
           <article className="max-w-3xl flex-2 space-y-6">
-            <h1 className="text-3xl font-bold md:max-w-xl lg:text-5xl">
+            <h1 className="text-3xl font-bold md:max-w-xl md:text-4xl lg:text-5xl">
               Encontre os melhores profissionais em um único local!
             </h1>
 
-            <p className="text-sm text-zinc-700 md:max-w-lg md:text-lg">
+            <p className="text-sm text-zinc-700 md:max-w-xl md:text-lg lg:max-w-lg">
               Nós somos uma plataforma para profissionais da saúde com foco em
               agilizar seu atendimento de forma simplificada e organizada.
             </p>
 
-            <Button asChild size="sm">
-              <Link href="#professionals">Profissionais disponíveis</Link>
+            <Button asChild>
+              <Link href="#clinics">Clínicas disponíveis</Link>
             </Button>
           </article>
 
